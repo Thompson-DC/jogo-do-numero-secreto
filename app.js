a03600ffce7f6e1ee139c127517535146c8c2eac
Thompson-DC <thompsondica@gmail.com>
2025-09-29 10:23:37 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+    console.log(`Número Secreto é ${numeroSecreto}`)
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Você acertou');
+        let nomeTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${nomeTentativas}!`
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        
+    } else { 
+        if (chute > numeroSecreto) {
+        exibirTextoNaTela('h1', 'Você errou');
+        exibirTextoNaTela('p', 'Digite um número menor!');
+        
+        } else {
+        exibirTextoNaTela('h1', 'Você errou');
+        exibirTextoNaTela('p', 'Digite um número maior!');
+        
+        }
+    tentativas++;
+    limparCampo()
+    }
+}
+
+console.log(tentativas);
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+}
